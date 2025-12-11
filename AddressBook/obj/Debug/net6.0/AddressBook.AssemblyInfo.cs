6fbfec77318af1450fc29572054140fc9f433a20
HaranathRaju <haranathraju09@gmail.com>
2025-12-11 14:17:42 +0530

    Added create contact feature

--- AddressBook/obj/Debug/net6.0/AddressBook.AssemblyInfo.cs
+++ AddressBook/obj/Debug/net6.0/AddressBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbf3e489a49aba4abb6862d7b9cba4e190962416")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273f8290e8bbcd49b627d0e0272e07d048a848cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
