ebe4beca4f66078c3482ec6c5cdf8ed3b060e71d
HaranathRaju <haranathraju09@gmail.com>
2025-12-11 15:18:25 +0530

    Added edit contact feature

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -13,6 +13,30 @@ namespace AddressBook
         public void addContact(Contact c)
         {
             contacts.Add(c);
+            Console.WriteLine("contact added successfully");
+        }
+
+        public void editContact(string firstname, string lastname, string address, string city, string state, string zip, string phonenumber, string email)
+        {
+
+            foreach (Contact i in contacts)
+            {
+                if (i.FirstName == firstname && i.LastName == lastname)
+                {
+                    i.Address = address;
+                    i.City = city;
+                    i.State = state;
+                    i.Zip = zip;
+                    i.PhoneNumber = phonenumber;
+                    i.Email = email;
+                    Console.WriteLine("contact edited succesfully");
+                    return;
+
+                }
+
+            }
+            Console.WriteLine("contact not found");
+        
 
         }
     }
--- AddressBook/obj/Debug/net6.0/AddressBook.AssemblyInfo.cs
+++ AddressBook/obj/Debug/net6.0/AddressBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+567f53a7e9f9964f645b62d4602b593bf1663335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f91cc40525d3f197aa2af086bc667578862dd6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
