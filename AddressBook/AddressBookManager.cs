561c678a728eb6cceaa21b07d9d097456a336c8f
HaranathRaju <haranathraju09@gmail.com>
2025-12-12 11:32:00 +0530

    Added multipleaddressbook feature

--- AddressBook/AddressBookManager.cs
+++ AddressBook/AddressBookManager.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace AddressBook
 {
-    internal class AddressBookManager
+    public  class AddressBookManager
     {
+        Dictionary<string,AddressBook> addressbooks=new Dictionary<string, AddressBook> ();
+
+        public void addAddressBook(string name)
+        {
+            if (addressbooks.ContainsKey(name))
+            {
+                Console.WriteLine("Address Book already exists!");
+                return;
+            }
+            addressbooks[name] = new AddressBook();
+            Console.WriteLine("Address Book created successfully");
+        }
+
+        public AddressBook getAddressBook(string name)
+        {
+            if (addressbooks.ContainsKey(name))
+            {
+                return addressbooks[name];
+
+            }
+            Console.WriteLine("Address Book not found!");
+            return null;
+        }
+
+        public void displayAddressBook()
+        {
+            if (addressbooks.Count==0)
+            {
+                Console.WriteLine("addressbooks is empty");
+                return;
+            }
+            foreach(var i in addressbooks.Keys)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
     }
 }
