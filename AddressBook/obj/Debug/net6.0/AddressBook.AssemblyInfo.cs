e3ee5cc7b4f4f4e00296a94df3941f9eb4b7322a
HaranathRaju <haranathraju09@gmail.com>
2025-12-12 11:49:28 +0530

    updated the uc6 code

--- AddressBook/obj/Debug/net6.0/AddressBook.AssemblyInfo.cs
+++ AddressBook/obj/Debug/net6.0/AddressBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2c1275e5fb018b4fc2b0f540cc4c00a6cdd4efb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fc769164306cf082cd2214cee07f07b0816babd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddressBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
